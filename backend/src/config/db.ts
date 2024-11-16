8105ff46da74708cc23489fa0948cd561bfd6897
Jose Arroyo <josearroyoescudero@gmail.com>
2024-11-16 14:44:14 +0100

    Separate prod & dev db connections

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,7 +2,17 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI_DEV!)
+    const env = process.env.NODE_ENV || 'development'
+    const uri =
+      env === 'production'
+        ? process.env.MONGO_URI_PROD
+        : process.env.MONGO_URI_DEV
+
+    if (!uri) {
+      throw new Error('Mongo URI not defined!')
+    }
+
+    const connection = await mongoose.connect(uri)
     console.log(`MongoDB Connected: ${connection.connection.host}`)
   } catch (error) {
     console.error('Error connecting to MongoDB: ', error)
