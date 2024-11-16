b486ba157488b59af31f7447a3e1d98cabef40af
Jose Arroyo <josearroyoescudero@gmail.com>
2024-11-16 12:51:50 +0100

    Create server

---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import app from './app'
+import { findAvailablePort } from './utils/portUtils'
+;(async () => {
+  try {
+    const port = await findAvailablePort(3000)
+    app.listen(port, () => {
+      console.log(`🚀 Server running on port ${port}`)
+    })
+  } catch (err) {
+    console.error('Error finding an available port: ', err)
+  }
+})()
