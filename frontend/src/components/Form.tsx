135a84ee977bd36929bca828f80a7d6a8a0952b1
Jose Arroyo <josearroyoescudero@gmail.com>
2024-11-17 15:44:50 +0100

    Responsive

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -13,64 +13,50 @@ const Form = () => {
   return (
     <>
       <form
-        className="w-full h-40 md:h-20 box-border flex flex-col md:flex-row gap-8 p-3"
+        className="w-full h-20 flex flex-col lg:flex-row lg:flex-wrap"
         onSubmit={handleSubmit}
       >
-        <div className="relative w-full">
-          <div
-            className="
-          absolute inset-0 start-0 flex items-center ps-4 md:ps-8 pointer-events-none
-        "
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="#ACAFB4"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="size-8 icon icon-tabler icons-tabler-outline icon-tabler-link"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M9 15l6 -6" />
-              <path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" />
-              <path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" />
-            </svg>
+        <div className="w-full lg:w-auto lg:grow order-1 md:order-1">
+          <div className="relative w-full mb-2 px-1">
+            <input
+              type="text"
+              className={`w-full p-3 ring-2 rounded-lg focus:outline-none 
+              focus:ring-2 required
+              ${
+                errorMessage
+                  ? "ring-errorBorder focus:ring-errorBorder"
+                  : "ring-secondary focus:ring-primary"
+              }`}
+              placeholder="https://www.google.com"
+              onChange={({ target }) => {
+                if (target.value) setErrorMessage("");
+                setUrl(target.value);
+              }}
+            />
           </div>
-          <input
-            type="text"
-            id="condenst-input"
-            className={`block w-full h-full mb-2 md:ml-5 ps-14 p-3 ring-2 rounded-lg 
-              focus:outline-none focus:ring-2 required
-            ${
-              errorMessage
-                ? "ring-errorBorder focus:ring-errorBorder"
-                : "ring-secondary focus:ring-primary"
-            }`}
-            placeholder="https://www.google.com"
-            onChange={({ target }) => {
-              if (target.value) setErrorMessage("");
-              setUrl(target.value);
-            }}
-          />
+        </div>
+        <div className="w-full order-2 lg:order-3 px-1 mb-4 lg:mb-0">
           {errorMessage ? (
-            <span className="md:ml-5 text-errorText/70 text-sm font-light">
+            <span className="text-errorText/70 text-sm font-light">
               {!url && errorMessage}
             </span>
           ) : (
             ""
           )}
         </div>
-        <button
-          className="flex-shrink-0 rounded-md px-6 py-3
-            bg-primary text-white font-semibold
-            hover:bg-tertiary active:shadow-inner active:translate-y-[2px]
-              ring-2 ring-primary hover:ring-tertiary"
-          type="submit"
-        >
-          Generate Short URL
-        </button>
+        <div className="w-full lg:w-auto lg:flex-none order-3 lg:order-2">
+          <div className="relative w-full mb-2 px-1">
+            <button
+              className="w-full p-3 rounded-lg focus:outline-none
+            bg-primary ring-2 ring-primary text-white font-semibold
+              active:shadow-inner active:translate-y-1 hover:ring-tertiary
+              hover:bg-tertiary"
+              type="submit"
+            >
+              Generate Short URL
+            </button>
+          </div>
+        </div>
       </form>
     </>
   );
