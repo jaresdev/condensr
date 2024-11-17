ff9ffa64821cf68c1c18b73d824f47b1bbecf193
Jose Arroyo <josearroyoescudero@gmail.com>
2024-11-17 17:15:17 +0100

    Add button to copy the url

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -87,8 +87,21 @@ const Form = () => {
         </form>
       )}
       {shortUrl && (
-        <div className="w-full text-center font-bold text-lg text-primary underline">
-          <a href={shortUrl} target="_blank">
+        <div className="w-full flex flex-col md:flex-row gap-5 items-center justify-center">
+          <button
+            className="p-3 rounded-lg focus:outline-none
+            bg-primary ring-2 ring-primary text-white font-semibold
+              active:shadow-inner active:translate-y-1 hover:ring-tertiary
+              hover:bg-tertiary"
+            onClick={() => navigator.clipboard.writeText(shortUrl)}
+          >
+            📋 Copy
+          </button>
+          <a
+            className="p-3 font-light text-lg lg:text-2xl text-tertiary underline"
+            href={shortUrl}
+            target="_blank"
+          >
             {shortUrl}
           </a>
         </div>
